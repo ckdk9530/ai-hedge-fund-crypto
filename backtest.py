1e6eb9a7e84e0c20642ab89a2fa386112e64916b
ckdk9530 <90123434+ckdk9530@users.noreply.github.com>
2025-06-22 19:38:15 +0800

    Use find_dotenv to load .env

--- backtest.py
+++ backtest.py
@@ -1,12 +1,12 @@
 import os
 import sys
 sys.path.append(os.path.join(os.path.dirname(__file__), "src"))
-from dotenv import load_dotenv
+from dotenv import load_dotenv, find_dotenv
 from src.utils import settings
 from src.backtest.backtester import Backtester
 
 
-load_dotenv()
+load_dotenv(find_dotenv())
 
 
 if __name__ == "__main__":
